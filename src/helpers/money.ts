d9f91fdcc39a2bf83a0b51532ff3366d0d5fa0b2
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-08-08 03:06:54 +0000

    feat: add products list and product items components

---
+++ src/helpers/money.ts
@@ -0,0 +1,8 @@
+const FormatToCurrency = (cents: number) => {
+    return new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(cents / 100);
+}
+ 
+export default FormatToCurrency;
